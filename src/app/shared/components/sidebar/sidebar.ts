b19980031044d5b764e3fa30f442ddca941ffcf0
JiaSengx <jslam1997@gmail.com>
2025-06-23 21:52:34 +0800

    created button and colors

--- src/app/shared/components/sidebar/sidebar.ts
+++ src/app/shared/components/sidebar/sidebar.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, computed, input, output } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faOutdent, faIndent } from '@fortawesome/free-solid-svg-icons';
+import { faOutdent, faIndent, faHome } from '@fortawesome/free-solid-svg-icons';
+import { Button } from "../button/button";
 
 @Component({
   selector: 'app-sidebar',
-  imports: [CommonModule, FontAwesomeModule],
+  imports: [CommonModule, FontAwesomeModule, Button],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss'
 })
@@ -14,6 +15,7 @@ export class Sidebar {
   toggle = output<void>();
 
   $toggleIcon = computed(() => (this.$isCollapse() ? faIndent : faOutdent));
+  home = faHome;
 
   onToggle() {
     this.toggle.emit();
